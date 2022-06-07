976d4d2974cc0b8c9dfda478c82bf0b144d9a2ef
ignachioz03 <icampot@santandertecnologia.com.ar>
2022-06-07 10:43:57 -0300

    act rama testing

--- src/__test__/editProduct.test.js
+++ src/__test__/editProduct.test.js
@@ -14,7 +14,7 @@ let producto = new Promise((resolve) =>{
         descripcion: "asdasd",
         opcionTienda: "mariscos",
         imagen: "https://www.w3schools.com/images/html5.gif",
-        stock: 4,
+        stock: 38,
         imagenes: [
             "https://www.w3schools.com/images/html5.gif"
         ],
@@ -40,14 +40,14 @@ test('Se trae un producto a modificar', async () =>{
         const inputNombre = screen.getByPlaceholderText(/Nombre/i);
         const inputValor = screen.getByPlaceholderText(/valor/i);
         const inputDescripcion = screen.getByPlaceholderText(/Descripcion/i);
-        const stockDisponible = screen.getByText('4')
+        const stockDisponible = screen.getAllByText('38')
         const inputImagenes = screen.getByAltText(/imagen/i);
 
-        expect(inputNombre).toMatch('test');
-        expect(inputValor).toMatch('123123');
-        expect(inputDescripcion).toMatch('asdasd');
+        expect(inputNombre.value).toMatch('test');
+        expect(inputValor.value).toMatch('123123');
+        expect(inputDescripcion.value).toMatch('asdasd');
         expect(screen.getByRole('option', {name: 'Ultramariscos Lolo Castro'}).selected).toBe(true);
         expect(inputImagenes.src).toMatch('https://www.w3schools.com/images/html5.gif');
-        expect(stockDisponible).toBeInTheDocument();
+        expect(stockDisponible[0]).toBeInTheDocument();
     })
 })
